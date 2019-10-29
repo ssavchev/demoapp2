3552bae513d37e7ad0e0dd1cdaa3c3e53255282a
ss <savchev@rtfglobal.com>
2019-10-29 11:25:13 -0400

    Initial Commit

---
+++ DemoApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp2
+//
+//  Created by Svetoslav Savchev on 2019-10-29.
+//  Copyright © 2019 SensorMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
