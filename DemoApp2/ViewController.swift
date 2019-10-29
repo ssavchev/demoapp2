0f06ecb5967c0edcf18440bc64541a069447c4d4
ss <savchev@rtfglobal.com>
2019-10-29 14:43:27 -0400

    First working prototype

--- DemoApp2/ViewController.swift
+++ DemoApp2/ViewController.swift
@@ -12,9 +12,15 @@ import Moya
 class ViewController: UIViewController {
     
     let provider = MoyaProvider<TestAPI>()
-
+    
+    @IBOutlet var testText: UILabel!
+    @IBOutlet var testButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        testText.isHidden = true
+        testButton.isHidden = true
         
         provider.request(.color) { [weak self] result in
             guard let self = self else { return }
@@ -22,21 +28,35 @@ class ViewController: UIViewController {
             switch result {
                 case .success(let response):
                     do {
-                        //let json = try response.mapJSON();
-                        //print(json)
                         let color = try response.map(APIColor.self)
                         self.updateAPIColor(color: color)
                     } catch {
-                        //print("exception")
                         self.updateBackgroundColor(color: UIColor.systemPink)
                     }
                 case .failure:
-                    //print("failure");
                     self.updateBackgroundColor(color: UIColor.systemRed)
             }
         }
     }
 
+    @IBAction func testButtonClicked(_ sender: Any) {
+        provider.request(.text) { [weak self] result in
+            guard let self = self else { return }
+
+            switch result {
+                case .success(let response):
+                    do {
+                        let text = try response.map(APIText.self)
+                        self.updateAPIText(text: text)
+                    } catch {
+                        self.updateBackgroundColor(color: UIColor.systemPink)
+                    }
+                case .failure:
+                    self.updateBackgroundColor(color: UIColor.systemRed)
+            }
+        }
+    }
+    
     func updateBackgroundColor(color: UIColor) {
         DispatchQueue.main.async {
             self.view.backgroundColor = color;
@@ -46,6 +66,14 @@ class ViewController: UIViewController {
     func updateAPIColor(color: APIColor) {
         DispatchQueue.main.async {
             self.view.backgroundColor = UIColor(red: color.red, green: color.green, blue: color.blue, alpha: color.alpha);
+            self.testButton.isHidden = false
+        }
+    }
+    
+    func updateAPIText(text: APIText) {
+        DispatchQueue.main.async {
+            self.testText.isHidden = false
+            self.testText.text = text.text;
         }
     }
 }
